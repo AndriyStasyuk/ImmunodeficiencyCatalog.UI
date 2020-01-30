8cf5e13f4d38fe943502410a3ffb177fe34567ad
AndriyStasyuk <andrii.stasiuk.o@gmail.com>
2020-01-30 21:45:05 +0200

    Update architecture

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment as environmentPromise } from './environments/environment';
+
+environmentPromise.then(environment => {
+  if (environment["production"]) {
+    enableProdMode();
+  }
+
+  platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
+
+});
