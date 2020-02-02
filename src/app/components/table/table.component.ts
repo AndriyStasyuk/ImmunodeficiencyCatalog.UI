6e493cf985ff2977d6ff29b4b75bcc8670879284
AndriyStasyuk <andrii.stasiuk.o@gmail.com>
2020-02-02 22:54:51 +0200

    Update table with info about patients

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -6,7 +6,7 @@ import {SelectionModel} from '@angular/cdk/collections';
 
 export interface PeriodicElement {
   id: string;
-  age: string;
+  age: number;
   date_of_diagnosis: string;
   ld_immunoglobulin: string;
   actual_route: string;
@@ -16,7 +16,21 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {id: 'ВОВ19000101', age: '1900-01-01', date_of_diagnosis: "1900-01-01", ld_immunoglobulin: '1900-01-01', 
+  {id: 'ВОВ19000101', age: 26, date_of_diagnosis: "1900-01-01", ld_immunoglobulin: '1900-01-01', 
+  actual_route: 'Підшкірно', dose: 23.32, producer: 'Spart', review: 'label'},
+  {id: 'ВОВ19000101', age: 25, date_of_diagnosis: "1900-01-01", ld_immunoglobulin: '1900-01-01', 
+  actual_route: 'Підшкірно', dose: 23.32, producer: 'Spart', review: 'label'},
+  {id: 'ВОВ19000101', age: 29, date_of_diagnosis: "1900-01-01", ld_immunoglobulin: '1900-01-01', 
+  actual_route: 'Підшкірно', dose: 23.32, producer: 'Spart', review: 'label'},
+  {id: 'ВОВ19000101', age: 15, date_of_diagnosis: "1900-01-01", ld_immunoglobulin: '1900-01-01', 
+  actual_route: 'Підшкірно', dose: 23.32, producer: 'Spart', review: 'label'},
+  {id: 'ВОВ19000101', age: 35, date_of_diagnosis: "1900-01-01", ld_immunoglobulin: '1900-01-01', 
+  actual_route: 'Підшкірно', dose: 23.32, producer: 'Spart', review: 'label'},
+  {id: 'ВОВ19000101', age: 25, date_of_diagnosis: "1900-01-01", ld_immunoglobulin: '1900-01-01', 
+  actual_route: 'Підшкірно', dose: 23.32, producer: 'Spart', review: 'label'},
+  {id: 'ВОВ19000101', age: 25, date_of_diagnosis: "1900-01-01", ld_immunoglobulin: '1900-01-01', 
+  actual_route: 'Підшкірно', dose: 23.32, producer: 'Spart', review: 'label'},
+  {id: 'ВОВ19000101', age: 25, date_of_diagnosis: "1900-01-01", ld_immunoglobulin: '1900-01-01', 
   actual_route: 'Підшкірно', dose: 23.32, producer: 'Spart', review: 'label'},
 ];
 
