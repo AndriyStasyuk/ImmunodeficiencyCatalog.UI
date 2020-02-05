f4db4ae1f08b0e65f53cb4b597060fae5f0ae493
SolyaHetman <solyahteman2013@gmail.com>
2020-02-05 23:43:00 +0200

    add form to registration

--- src/app/components/patient-components/diagnosis-pid/diagnosis-pid.component.ts
+++ src/app/components/patient-components/diagnosis-pid/diagnosis-pid.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiagnosisPidComponent implements OnInit {
 
+  // Data for radio button  
+  damagedGenes: string;
+  damagedGeness: string[] = ['Історія генетичних досліджень невідома', 'Генетичне дослідження не проводилось','Генетичне дослідження проводилось,мутації не виявлено'];
+  
+  geneticResearchDate: string;
+  geneticResearchDates: string[] = ['Невідомо', 'Відомо'];
+  
+  sequencingMethod: string;
+  sequencingMethods: string[] = ['Секвенування гена', 'Екзом/геном секвенування','Невідомо'];
+
+  geneticResearchReason: string;
+  geneticResearchReasons: string[] = ['Специфічні клінічні симптоми', 'Сімейний скринінг','Пренатальна діагностика', 'Невідомо']; 
   constructor() { }
 
   ngOnInit() {
--- src/app/components/patient-components/stemcells/stemcells.component.ts
+++ src/app/components/patient-components/stemcells/stemcells.component.ts
@@ -6,8 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stemcells.component.scss']
 })
 export class StemcellsComponent implements OnInit {
+// Data for radio button  
 
-  constructor() { }
+stemCellsTransplantation: string;
+stemCellsTransplantations: string[] = ['Так', 'Ні','Невідомо'];
+
+transplantationDate: string;
+transplantationDates: string[] = ['Відомо', 'Невідомо'];
+
+CB14_soure: string;
+CB14_soures: string[] = ['Кістковий мозок', 'Периферична кров','Пуповинна кров','Невідомо'];
+  
+geneticTherapy: string;
+geneticTherapys: string[] = ['Так','Ні','Невідомо']
+
+constructor() { }
 
   ngOnInit() {
   }
