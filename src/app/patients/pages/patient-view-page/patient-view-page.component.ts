161bf3059dc1285871676869d969c575576a1cb7
SolyaHetman <solyahteman2013@gmail.com>
2020-03-08 23:44:37 +0200

    fix some style

--- src/app/patients/pages/patient-view-page/patient-view-page.component.ts
+++ src/app/patients/pages/patient-view-page/patient-view-page.component.ts
@@ -3,6 +3,7 @@ import { switchMap } from 'rxjs/operators';
 import { PatientService } from './../../../services/patient.service';
 import { ActivatedRoute } from '@angular/router';
 import { ImmunoglobulinReplacementTherapyInfo } from './../../../models/imm-replacement-therapy-patien-info';
+import { StemCellsTransplantation } from 'src/app/models/stem-cells-transplantation-info';
 
 
 @Component({
@@ -19,6 +20,7 @@ export class PatientViewPageComponent implements OnInit {
      data: any[] ;
 
   rit_info: ImmunoglobulinReplacementTherapyInfo = new ImmunoglobulinReplacementTherapyInfo()
+  stem_cells_transplantation: StemCellsTransplantation = new StemCellsTransplantation()
   ngOnInit() {
     this.activatedRoute.params.pipe(switchMap(routeParams =>this.patientService.getPatientById(routeParams['id'])))
     .subscribe( response => console.log(this.data = response), error => console.log(error));
